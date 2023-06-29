c0ec1abf52162acf4312a191d7b29d218a9f462b
nahla-educate <nehla.ar@gmail.com>
2023-06-29 18:31:35 +0100

    RA

---
+++ Assets/Bouton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouton : MonoBehaviour
+{
+    public void buttonFunction(string buttonLink)
+    {
+        Application.OpenURL(buttonLink);
+    }
+}
+
---
+++ Assets/VBtnFn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia; 
+
+
+public class VBtnFn : MonoBehaviour
+{
+    public GameObject vbBtnObj;
+    public GameObject obj1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("VirtualButton");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Pressed");
+        obj1.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Released");
+        obj1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
